6e01b8b399b3700108a56d0b1786238b4cc12ce5
Johanna Mantilla Duque <johanna1431@gmail.com>
2015-06-19 14:10:41 -0500

    Finish integration bootstrap in the Authy views

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
-  devise_scope :user do
-    get "/", to: "devise/sessions#new"
-  end
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
