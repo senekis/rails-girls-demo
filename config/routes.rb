1f60a521e3bb2e3f8f66f2de6620b85afe6fd49a
Johanna Mantilla Duque <johanna1431@gmail.com>
2015-06-18 20:21:32 -0500

    Include authy-devise in the user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => {
+    :verify_authy => "/verify-token",
+    :enable_authy => "/enable-two-factor",
+    :verify_authy_installation => "/verify-installation"
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
